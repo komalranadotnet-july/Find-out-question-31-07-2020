df6bd6d6c8ca5551799f0d6c5e0fb9a2f115ecf8
komal rana <komal.rana@zensar.com>
2020-08-03 09:22:48 +0530

    Find out question upload

---
+++ StringBuilderMethodExapmle/StringBuilderMethodExapmle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderMethodExapmle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder("Hello World!", 50);
+            for (int i = 0; i < sb.Length; i++)
+                Console.Write(sb[i]);
+
+
+            StringBuilder sb1 = new StringBuilder();
+            sb.Append("Hello ");
+            sb.AppendLine("World!");
+            sb.AppendLine("Hello C#");
+            Console.WriteLine(sb);
+          
+            
+            StringBuilder sbAmout = new StringBuilder("Your total amount is ");
+            sbAmout.AppendFormat("{0:C} ", 25);
+            Console.WriteLine(sbAmout);
+
+            StringBuilder sb2 = new StringBuilder("Hello World!", 50);
+            sb.Remove(6, 7);
+
+
+            StringBuilder sb3 = new StringBuilder("Hello World!");
+            sb.Replace("World", "C#");
+            Console.WriteLine(sb);
+
+
+        }
+    }
+}
---
+++ StringClassMethods/StringClassMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string   firstname = " STEVEN CLARK ";
+          string   lastname = "Clark";
+
+            Console.WriteLine(firstname.Contains("ven")); //Check whether specified value exists or not in string
+           
+            Console.WriteLine(firstname.IndexOf("e")); //Returns the first index position of specified value
+           
+            Console.WriteLine(firstname.ToLower()); //Covert string into lower case
+         
+            Console.WriteLine(lastname.ToUpper());//Convert string into Upper case
+          
+            Console.WriteLine(firstname.Length);//Returns the Length of String
+          
+            Console.WriteLine(firstname.Trim());//It removes starting and ending white spaces from string.
+
+
+        }
+    }
+}
